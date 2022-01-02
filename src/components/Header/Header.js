93807733aa1de6d671a854b770728034ed75f61a
Joseph Maramba <josephmaramba50@gmail.com>
2022-01-02 13:54:14 +0300

    furthe styling - nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,15 @@ const Header = ({ isDark, handleIsDark }) => {
 	return (
 		<header className={isDark ? `dark-header header` : `header`}>
 			<h2 className="logo">Where in the world?</h2>
-			<h3 className="dark-mode-icon" onClick={handleIsDark}>
-				<i className="far fa-moon"></i> Dark Mode
-			</h3>
+			{isDark ? (
+				<h3 className="dark-mode-icon" onClick={handleIsDark}>
+					<i className="fas fa-sun"></i> Light Mode
+				</h3>
+			) : (
+				<h3 className="dark-mode-icon" onClick={handleIsDark}>
+					<i className="far fa-moon"></i> Dark Mode
+				</h3>
+			)}
 		</header>
 	);
 };
