e49e038cd3c9501d04595df60a8a559e2eecab69
Joseph Maramba <josephmaramba50@gmail.com>
2022-05-24 07:04:35 +0300

    animation fixes

--- src/components/Country/CountryItem.js
+++ src/components/Country/CountryItem.js
@@ -5,28 +5,26 @@ import CountryStats from "./CountryStats";
 import { useNavigate } from "react-router-dom";
 
 const CountryItem = ({ isDark, country }) => {
-	const navigate = useNavigate();
-	return (
-		<motion.div
-			animate={{ opacity: 1, }}
-      		initial={{ opacity: 0, }}
-      		exit={{ opacity: 0, }}
-      		transition={{ duration: 0.2 }}
-      		layout
-			className={isDark ? `dark-country country` : `country`}
-			onClick={() => navigate(`/country/${country.name.toLowerCase()}`)}
-		>
-			<img
-				className="country__flag"
-				src={country.flags.svg}
-				alt={country.name}
-			/>
-			<div className="country__info">
-				<h4 className="country__name">{country.name}</h4>
-				<CountryStats country={country} />
-			</div>
-		</motion.div>
-	);
+  const navigate = useNavigate();
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      layout
+      className={isDark ? `dark-country country` : `country`}
+      onClick={() => navigate(`/country/${country.name.toLowerCase()}`)}
+    >
+      <motion.img
+        className="country__flag"
+        src={country.flags.svg}
+        alt={country.name}
+      />
+      <motion.div className="country__info">
+        <motion.h4 className="country__name">{country.name}</motion.h4>
+        <CountryStats country={country} />
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default CountryItem;
