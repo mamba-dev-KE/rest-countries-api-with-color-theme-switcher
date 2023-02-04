1804568b335cb409dcf52d5050f5bbd85221942c
Joseph Maramba <joseph@smartmile.eu>
2023-02-04 10:35:42 +0300

    fix dark mode not working

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,24 @@
+import { useDarkMode } from 'hooks';
 import './Header.css';
 
 const Header = () => {
-  // const { isDark, handleIsDark } = useContext(ColorSchemeContext);
-
-  const isDark = false;
-  const handleIsDark = () => {}
+  const { isDark, toggle } = useDarkMode();
 
   return (
     <header className={isDark ? `dark-header header` : `header`}>
       <div className="header-container">
         <h2 className="logo">Where in the world?</h2>
         {isDark ? (
-          <h3 className="dark-mode-icon" onClick={handleIsDark}>
+          <h3 className="dark-mode-icon" onClick={toggle}>
             <i className="fas fa-sun"></i> Light Mode
           </h3>
         ) : (
-          <h3 className="dark-mode-icon" onClick={handleIsDark}>
+          <h3 className="dark-mode-icon" onClick={toggle}>
             <i className="fas fa-moon"></i> Dark Mode
           </h3>
         )}
       </div>
-    </header>   
+    </header>
   );
 };
 
